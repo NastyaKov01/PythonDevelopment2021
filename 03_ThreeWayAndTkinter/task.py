c2a10a9e7e4d021c1766a4328851343849abfdf7
Kovalenko <s2180445@gse.cs.msu.ru>
2021-03-12 14:36:51 +0300

    Buttons can be shuffled
    The creation of buttons rewritten

--- 03_ThreeWayAndTkinter/task.py
+++ 03_ThreeWayAndTkinter/task.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from random import shuffle
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -6,16 +7,28 @@ class Application(tk.Frame):
         self.grid()
         self.createWidgets()
 
+    def randHandler(self):
+        shuffle(self.numList)
+        for i in range(15):
+            b, k = self.buttons[i], self.numList.index(i)
+            b.grid(row = 1 + k//4, column = k%4, sticky = "nsew")
+
+
     def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.nextButton = tk.Button(self, text='Next')
-        self.quitButton.grid()
-        self.nextButton.grid()
-        but= "Button"
-        for i in range(1, 16):
-            name = but + str(i)
-            self.name = tk.Button(self, text=str(i))
-            self.name.grid()
+        self.numList, self.buttons = [], []
+        self.quitButton = tk.Button(self, text = 'Quit', command = self.quit)
+        self.newButton = tk.Button(self, text = 'New', command = self.randHandler)
+        for i in range(15):
+            self.numList.append(i)
+            name = "Button" + str(i+1)
+            self.name = tk.Button(self, text=str(i+1))
+            self.buttons.append(self.name)
+        self.numList.append(15)
+
+        self.quitButton.grid(row = 0, column = 2, columnspan = 2)
+        self.newButton.grid(row = 0, column = 0, columnspan = 2)
+        self.randHandler()
+
 
 app = Application()
 app.master.title('15')
